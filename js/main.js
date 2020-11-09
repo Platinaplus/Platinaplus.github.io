18c0f24b3861e57aaaaacfbee5455014c1d0aff6
Platina <prokofievan@yahoo.com>
2020-11-09 15:12:44 +0300

    final commit

--- js/main.js
+++ js/main.js
@@ -10,11 +10,35 @@ $(function () {
     $(".open").toggleClass("hidden");
   });
 
-  $(".menu a, .footer a, .logo").on("click", function (e) {
+  $(".menu a, .footer a, .logo, .header__icon").on("click", function (e) {
     e.preventDefault();
     var id = $(this).attr("href"),
       top = $(id).offset().top;
     $("body,html").animate({ scrollTop: top }, 1500);
+  });
+
+  const btnShowMore = document.querySelector(".gallery__more");
+  const container = document.querySelector(".gallery__photo");
+
+  btnShowMore.addEventListener("click", function (e) {
+    e.preventDefault();
+
+    if (btnShowMore.dataset.option == "hidden") {
+      container.classList.add("add");
+      btnShowMore.dataset.option = "visible";
+      btnShowMore.innerText = "Скрыть";
+    } else {
+      container.classList.remove("add");
+      btnShowMore.dataset.option = "hidden";
+      btnShowMore.innerText = "Показать еще";
+    }
+  });
+
+  $(window).scroll(function () {
+    $(".header__top").toggleClass(
+      "scroll",
+      $(this).scrollTop() > $(".header").outerHeight()
+    );
   });
 
   var mixer = mixitup(".gallery__photo");
--- js/script.js
+++ js/script.js
@@ -1,25 +1,53 @@
 window.addEventListener("DOMContentLoaded", function () {
-const btnShowMore = document.querySelector('.gallery__more');
-const hiddenElements = document.querySelectorAll('.gallery__item--hidden');
+
+  const btnShowMore = document.querySelector('.gallery__more');
+  const hiddenElements = document.querySelectorAll('.gallery__item--hidden');
 
 btnShowMore.addEventListener('click', function(e){
   e.preventDefault();
 
   if (btnShowMore.dataset.option == 'hidden') {
     hiddenElements.forEach(element => {
-    element.style.display = 'block';
+      element.classList.remove('gallery__item--hidden');
+    
   })
   btnShowMore.dataset.option = 'visible';
   btnShowMore.innerText = 'Скрыть';
 }
   else if (btnShowMore.dataset.option == 'visible') {
     hiddenElements.forEach(element => {
-      element.style.display = 'none';
+      element.classList.add('gallery__item--hidden');
     });
     btnShowMore.dataset.option = 'hidden';
     btnShowMore.innerText = 'Показать еще';
 }
 });
+
+const buttons = document.querySelectorAll('.gallery__btn')
+const photos = document.querySelectorAll('.gallery__item')
+
+buttons.forEach(button => {
+  button.addEventListener('click', function(){
+    let filter = button.dataset.filter;
+    photos.forEach(photo => {
+      if (filter == "all") {
+        if (!photo.classList.contains('gallery__item--hidden')){
+          photo.style.display = "block";
+        }
+      }
+      else if (photo.classList.contains(filter.slice(1))){
+        if (!photo.classList.contains('gallery__item--hidden')){
+          photo.style.display = "block";
+        }
+        
+      } else {
+        photo.style.display = "none";
+      }
+    })
+
+    });
+  });
+
 });
 
 
